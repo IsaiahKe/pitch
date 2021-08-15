76bff8b60189c97570f83932dac09256e46edd0f
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-15 15:19:54 +0300

    Add authrntication forms file

---
+++ app/auth/forms.py
@@ -0,0 +1,8 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField,SubmitField
+# from wtforms.fields.simple import PasswordField
+# from wtforms.validators import required,email_validator
+# class UserCredential(FlaskForm):
+#     username=StringField(label='Username Email',validators=[required,email_validator])
+#     password=PasswordField(label="Password",validators=[required])
+#     submit=SubmitField

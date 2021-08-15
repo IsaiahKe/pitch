a1fc19fb251e263ca5750ac6302f40120304b69c
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-15 15:02:43 +0300

    Add pitch model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class User(db.Model):
     def __repr__(self):
         return f'User{self.username}'
     
+class Pitch(db.Model):
+    id=db.Column(db.Interger,primary_key=True)
+    content=db.Column(db.String(255))
+    likes=db.Column(db.Integer(4))
+    dislikes=db.Column(db.Integer(4))
+    
+    def __repr__(self):
+        return f'Pitch{self.content}'

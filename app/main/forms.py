6670cc2fc9b9039133ba808581d29e58eeb6808b
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-18 23:08:34 +0300

    add main forms

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField,SubmitField,TextField
+from wtforms.validators import Required
+class ProfileUpdate(FlaskForm):
+    bio=TextAreaField(label="Bio Data",validators=[Required()])
+    
+    save=SubmitField('Save')
+    
+class PitchForm(FlaskForm):
+    category=TextField(label="Category",validators=[Required()])
+    content=TextAreaField(label='Pitch',validators=[Required()])
+    submit=SubmitField('Post')
+    
+    
+class CommentForm(FlaskForm):
+    comment=TextAreaField("Comment",validators=[Required()])
+    submit=SubmitField('Send')

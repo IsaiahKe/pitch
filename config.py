979c254062132de2ae62106047fa745734e2f7d5
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-18 13:56:09 +0300

    Add configurations

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://izzie:Access@localhost/pitch'
+    UPLOADED_PHOTOS_DEST='app/static/img'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class DevConfig(Config):

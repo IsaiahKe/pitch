1e239efad094278101e260f1deea4a8e275f3b95
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-18 23:19:05 +0300

    update forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,8 +1,28 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,SubmitField
-# from wtforms.fields.simple import PasswordField
-# from wtforms.validators import required,email_validator
-# class UserCredential(FlaskForm):
-#     username=StringField(label='Username Email',validators=[required,email_validator])
-#     password=PasswordField(label="Password",validators=[required])
-#     submit=SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField,PasswordField,ValidationError,BooleanField,TextAreaField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+
+
+
+class UserRegistration(FlaskForm):
+    username=StringField('Username',validators=[Required()])
+    email=StringField('User Email',validators=[Required(),Email()])
+    bio=TextAreaField(label="Biography",validators=[Required()])
+    password=PasswordField("User password",validators=[Required(),EqualTo('password_confirm',message="Passwords Dont match!")])
+    password_confirm=PasswordField(label="Confirm Password",validators=[Required()])
+    submit=SubmitField('Register')
+    
+    def check_email(self,data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('There Email is already used')
+        
+    def check_username(self,data_field):
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError("The Username is already in use")
+        
+class UserLogin(FlaskForm):
+    email=StringField('Enter user email', validators=[Required(),Email()])
+    password=PasswordField('Enter password',validators=[Required()])
+    remember=BooleanField('Remember me')
+    submit=SubmitField('Continue')

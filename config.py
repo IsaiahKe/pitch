1dfabc192ed16c552ab70ff858f2bacf695ad4e7
IsaiahKe <isaiah.keoye@student.moringaschool.com>
2021-08-19 15:49:36 +0300

    Deployment to heroku

--- config.py
+++ config.py
@@ -18,10 +18,9 @@ class DevConfig(Config):
     DEBUG = True
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
-    # if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
-    #      SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://','postgresql://',1)
-    pass
+    SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+          SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://','postgresql://',1)
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://izzie:Access@localhost/pitch-test'
 
